a77bb661c6bacfd89906892c2bc1e519a1b6aa70
Gabriel Ken Kudo de Lima <gabkenkudo@gmail.com>
2026-03-01 16:08:54 -0300

    Primeira versão dos códigos.

---
+++ Área do triangulo.py
@@ -0,0 +1,7 @@
+print("Calculando área do triângulo...")
+print ("Digite o tamanho da altura do triângulo:")
+altura = float(input())
+print("Digite o tamanho da base do triângulo:")
+base = float(input())
+area = (base*altura) / 2
+print("A área do triângulo de altura" , altura , "e base" , base , "equivale a:" , area)
